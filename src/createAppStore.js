f100c6ac65c74c1ba8adf89875d6d136e90c2af8
Lee Richmond <LRichmond@signstix.com>
2017-02-16 15:19:20 +0000

    remove redux search

--- src/createAppStore.js
+++ src/createAppStore.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import initialState from './reducers/initialState';
-import { reduxSearch } from 'redux-search';
 import thunk from 'redux-thunk';
 
 export default function createAppStore(initialState) {
@@ -9,13 +8,5 @@ export default function createAppStore(initialState) {
     rootReducer,
     initialState,
     applyMiddleware(thunk),
-    reduxSearch({
-      resourceIndexes: {
-        users: ['firstName', 'lastName', 'email'],
-      },
-      resourceSelector: (resourceName, state) => {
-        return state.resources.get(resourceName);
-      }
-    })
   );
 }
