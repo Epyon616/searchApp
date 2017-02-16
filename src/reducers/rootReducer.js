3d28b5ef5ba7d76d88f4018bd460e5ca2a1f2754
Lee Richmond <LRichmond@signstix.com>
2017-02-16 15:20:05 +0000

    remove search
    since I am no longer trying to use redux-search it makes sense to do a
    bit of housekeeping this means removing any related code from
    initialState and rootReducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,9 +1,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import { reducer as searchReducer } from 'redux-search';
 import usersReducer from './userReducer';
 
 const rootReducer = combineReducers({
-  search: searchReducer,
   users: usersReducer,
 });
 
